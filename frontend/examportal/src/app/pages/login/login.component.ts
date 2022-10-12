f3f42555330a884cfc1c93e8ded251e59c7fbf42
Abhishek shaw <abhishekshaw813@gmail.com>
2022-10-12 21:47:43 +0530

    frontend token auth complete

--- frontend/examportal/src/app/pages/login/login.component.ts
+++ frontend/examportal/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  loginData={
+    username:'',
+    password:''
+  }
 
-  constructor() { }
+  constructor(private snack:MatSnackBar,private login:LoginService) { }
 
   ngOnInit(): void {
   }
+
+  formSubmit(){
+    if(this.loginData.username.trim()=='' || this.loginData.username==null){
+      this.snack.open("username is required !!",'ok',{
+        duration:3000,
+      });
+      return;
+    }
+    if(this.loginData.password.trim()=='' || this.loginData.password==null){
+      this.snack.open("password is required !!",'ok',{
+        duration:3000,
+      });
+      return;
+    }
+    //request server to generate token
+    this.login.generateToken(this.loginData).subscribe({
+      next:(data)=>{
+        console.log("success login");
+        console.log(data);
+      },
+      error:(e)=>{
+        console.log("login error")
+        console.log(e);
+      }
+    })
+
+
+    }
 
 }
---
+++ frontend/examportal/src/app/services/login.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  //generate token
+  public generateToken(loginData: any) {
+    return this.http.post(`${baseUrl}/generate-token`, loginData);
+
+  }
+
+  //login user : set token in localstorage
+  public loginUser(token: any) {
+    localStorage.setItem('token', token);
+    return true;
+  }
+  //islogin: user is logged in or not
+  public isLoggedIn() {
+    let tokenStr = localStorage.getItem("token")
+    if (tokenStr == undefined || tokenStr == '' || tokenStr == null) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  // Logout : remove token from local storage
+  public logout() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    return true;
+  }
+  // get token 
+  public getToken() {
+    return localStorage.getItem('token');
+  }
+  //SetUserDetails
+  public setUser(user:any){
+    localStorage.setItem('user',JSON.stringify(user));
+  }
+  //getUserDetails
+  public getUser(){
+   let userStr =  localStorage.getItem('user');
+   if(userStr!=null){
+    return JSON.parse(userStr);
+   }else{
+    this.logout();
+    return null;
+   }
+  }
+  //getuserAuthority
+  public getUserRole(){
+    let user = this.getUser()
+    return user.authorities[0].authority;
+  }
+
+
+}
